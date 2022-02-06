c7999a9adb59d1daf364fee79e0ccff12db953a4
BoltManGuy <boltsoft@gmail.com>
2022-02-06 04:41:53 -0700

    uhhhh... add ultimate overkill mode

--- reframework/autorun/UI_Scaler.lua
+++ reframework/autorun/UI_Scaler.lua
@@ -5,7 +5,7 @@
 
 local settings = {
 	uiScale = 0.75;
-	wirebugScale = 1.0;
+	centerScale = 0.75;
 	mapScale = 0.8;
 	bottomRightScale = 0.75;
 
@@ -18,7 +18,7 @@ local settings = {
 local elementDatas = {
 	["snow.gui.GuiHud"] = {},
 	["snow.gui.GuiHud_QuestTarget"] = {},
-	["snow.gui.GuiHud_TimeLimit"] = {},
+	["snow.gui.GuiHud_Timelimit"] = {},
 	["snow.gui.GuiLobbyQuestInfoWindow"] = {},
 	["snow.gui.GuiHud_Sharpness"] = {},
 	["snow.gui.GuiHud_Weapon_L_Swd"] = {},
@@ -26,10 +26,10 @@ local elementDatas = {
 	["snow.gui.GuiHud_Weapon_S_Axe"] = {},
 	["snow.gui.GuiHud_Weapon_G_Lan"] = {},
 	["snow.gui.GuiHud_Weapon_Horn"] = {},
-	["snow.gui.GuiHud_Weapon_I_Glaive"] = {isGlaive = true},
+	["snow.gui.GuiHud_Weapon_I_Glaive"] = {isComplex = true},
 	["snow.gui.GuiHud_Weapon_Ham"] = {},
 	["snow.gui.GuiHud_Weapon_D_Bld"] = {},
-	["snow.gui.GuiHud_Weapon_Bowgun"] = {useScale = true},
+	["snow.gui.GuiHud_Weapon_Bowgun"] = {isComplex = true},
 	["snow.gui.GuiQuestHudMapWindow"] = {anchor = "LeftBottom", isMap = true},
 	["snow.gui.GuiQuestHudBulletSlider"] = {anchor = "RightBottom"},
 	["snow.gui.GuiHud_ItemActionSlider"] = {anchor = "RightBottom"},
@@ -37,23 +37,113 @@ local elementDatas = {
 	["snow.gui.GuiProgressInfo"] = {anchor = "RightTop"},
 	["snow.gui.GuiQuestHudCustomShortCircle"] = {anchor = "RightCenter"},
 
-
-	--most of the issues below could be solved by moving the sub ui panels directly but thats really annoying to set up so 
-	--it is what it is atm
-
-	--you can try to turn this on but it makes the target reticle a bit off
-	--id recommend just turning the target hud off entirely
-	-- ["snow.gui.GuiHud_TgCamera"] = {isTargetElement = true, anchor = "RightTop"},
-
-	--this ones a bit tricky since the bugs are on the same object as the reticle and it doesnt scale quite right
-	--as a compromise im just shifting the reticle to the right so its still center
-	--not sure if this will cause issues on other resolutions or something tho
-	["snow.gui.GuiHud_HunterWire"] = {useScale = true, isWireB = true, anchor = "CenterCenter", offsetX = 0.25},
-
-	--unfortunately this doesnt scale properly and is also for player names which are in world space
-	--but this might be fine if you turn player names off anyway
-	-- ["snow.gui.GuiCommonHeadMessage"] = {anchor = "CenterCenter"}, 
+	["snow.gui.GuiHud_TgCamera"] = {isComplex = true, anchor = "RightTop"},
+	["snow.gui.GuiHud_HunterWire"] = {isComplex = true, anchor = "CenterBottom"},
+	["snow.gui.GuiCommonHeadMessage"] = {isComplex = true, anchor = "CenterCenter"}, 
 };
+
+
+function SetDefaultSubPanels()
+	local eName = "snow.gui.GuiHud_Weapon_I_Glaive";
+	if not settings.elementSettings[eName].subPanels then
+		settings.elementSettings   [eName].scale = 1;
+		settings.elementSettings   [eName].subPanels = {
+			pnl_I_Gla_Top = {
+				anchor = 0.0;
+				posAdjustX = 0.15;
+				posAdjustY = 0.1;
+				posX = 0.0;
+				posY = 0.0;
+				scale = 1.0;
+				useGlobalScale = true;
+			},
+		};
+	end
+
+	eName = "snow.gui.GuiHud_TgCamera";
+	if not settings.elementSettings[eName].subPanels then
+		settings.elementSettings   [eName].scale = 1;
+		settings.elementSettings   [eName].subPanels = {
+			_tgCameraIconPanel = {
+				anchor = 6;
+				posAdjustX = 1;
+				posAdjustY = 0;
+				posX = 0.0;
+				posY = 0.0;
+				scale = 1.0;
+				useGlobalScale = true;
+			},
+		};
+	end
+
+	eName = "snow.gui.GuiCommonHeadMessage";
+	if not settings.elementSettings[eName].subPanels then
+		settings.elementSettings   [eName].scale = 1;
+		settings.elementSettings   [eName].subPanels = {
+			_pnl_HeadUI = {
+				anchor = 4;
+				posAdjustX = 0;
+				posAdjustY = 0;
+				posX = 0.0;
+				posY = 0.0;
+				scale = 1.0;
+				useGlobalScale = true;
+			},
+		};
+	end
+
+	eName = "snow.gui.GuiHud_HunterWire";
+	if not settings.elementSettings[eName].subPanels then
+		settings.elementSettings   [eName].scale = 1;
+		settings.elementSettings   [eName].subPanels = {
+			pnl_HunterWire = {
+				anchor = 5;
+				posAdjustX = 1;
+				posAdjustY = 0.925;
+				posX = 0.0;
+				posY = 0.0;
+				scale = 1.0;
+				useGlobalScale = true;
+				absolutePos = true;
+			},
+			pnl_WireAiming = {
+				anchor = 4;
+				posAdjustX = 0;
+				posAdjustY = 0;
+				posX = 0.0;
+				posY = 0.0;
+				scale = 1.0;
+				useGlobalScale = true;
+			},
+		};
+	end
+
+	eName = "snow.gui.GuiHud_Weapon_Bowgun";
+	if not settings.elementSettings[eName].subPanels then
+		settings.elementSettings   [eName].scale = 1;
+		settings.elementSettings   [eName].subPanels = {
+			_BowgunBulletPanel = {
+				anchor = 0;
+				posAdjustX = 0.075;
+				posAdjustY = 0.1;
+				posX = 0.0;
+				posY = 0.0;
+				scale = 1.0;
+				useGlobalScale = true;
+			},
+			_BowgunSpecialPosPanel = {
+				anchor = 8;
+				posAdjustX = 1;
+				posAdjustY = 0.3;
+				posX = 0.0;
+				posY = 0.0;
+				scale = 1.0;
+				useGlobalScale = true;
+			},
+		};
+	end
+
+end
 
 local anchors = {
 	["LeftTop"     ] = 0,
@@ -79,14 +169,28 @@ local dirScales = {
 	[8] = { 1,  1, "RightBottom" },
 };
 
+local subDirScales = {
+	[0] = { -1, -1, "LeftTop"     },
+	[1] = { -1, .5, "LeftCenter"  },
+	[2] = { -1,  1, "LeftBottom"  },
+	[3] = { .5, -1, "CenterTop"   },
+	[4] = { .5, .5, "CenterCenter"},
+	[5] = { .5,  1, "CenterBottom"},
+	[6] = {  1, -1, "RightTop"    },
+	[7] = {  1, .5, "RightCenter" },
+	[8] = {  1,  1, "RightBottom" },
+};
+
 
 local elementIdxs = {};
+
+function GetElementClassDisplayName(typeString)
+	return typeString:sub(10):gsub("GuiHud_", ""):gsub("Gui", "");
+end
 
 function GetElementSettings(typeString)
 
 	local elementSettings = settings.elementSettings[typeString];
-
-	-- elementSettings = nil;
 
 	if not elementSettings then
 
@@ -97,7 +201,7 @@ function GetElementSettings(typeString)
 		end
 
 		elementSettings = {
-			displayName = typeString:sub(10):gsub("GuiHud_", ""):gsub("Gui", "");
+			displayName = GetElementClassDisplayName(typeString);
 			scale = settings.uiScale;
 			posX = 0;
 			posY = 0;
@@ -121,8 +225,24 @@ function LoadSettings()
 	end
 
 	if not settings.mapScale then settings.mapScale = 0.8; end
+	if not settings.centerScale then settings.centerScale = 0.75; end
 	if not settings.bottomRightScale then settings.bottomRightScale = settings.uiScale; end
-	if not settings.elementSettings then settings.elementSettings = {}; end
+	if not settings.elementSettings then settings.elementSettings = {}; end	
+
+	for key, element in pairs(settings.elementSettings) do
+		if not elementDatas[key] then
+			elementDatas[key] = {}
+		end
+	end
+
+	SortElements();
+
+	SetDefaultSubPanels();
+end
+
+function SortElements()
+
+	elementIdxs = {};
 
 	for key, element in pairs(elementDatas) do
 		table.insert(elementIdxs, key);
@@ -164,14 +284,10 @@ local set_Position = transformType:get_method("set_Position");
 local get_Position = transformType:get_method("get_Position");
 
 local vec3Type = sdk.find_type_definition("via.vec3");
-local vec3X = vec3Type:get_field("x");
-local vec3Y = vec3Type:get_field("y");
 local vecCtor = vec3Type:get_method(".ctor(System.Single, System.Single, System.Single)");
 local vecGetItem = vec3Type:get_method("get_Item");
 local vecAdd = vec3Type:get_method("op_Addition");
 local vecSub = vec3Type:get_method("op_Subtraction");
-local vecDiv = vec3Type:get_method("op_Division");
-local vecMult = vec3Type:get_method("op_Multiply");
 
 local sizeType = sdk.find_type_definition("via.Size");
 local sizeCtor = sizeType:get_method(".ctor(System.Single, System.Single)");
@@ -191,7 +307,6 @@ local mItems = behaviourListType:get_field("mItems");
 
 local behaviourType = sdk.find_type_definition("snow.gui.GuiRootBaseBehavior");
 local behaviourTypeSystem = sdk.typeof("snow.gui.GuiRootBaseBehavior");
-local bToString = behaviourType:get_method("ToString()");
 local view_root = behaviourType:get_field("view_root");
 
 local viewType = sdk.find_type_definition("via.gui.View");
@@ -216,11 +331,18 @@ local invScale = 0;
 local tmpScale = 1;
 local ramIdx = -1;
 
-function GuiListIterate()
-
+function GetElementList()
 	local behaviourTable = _guiSystemBehaviorTbl:get_data(GetGuiManager());
 	local count = _count:get_data(behaviourTable);
 	local entries = _entries:get_data(behaviourTable);
+
+	return entries, count;
+end
+
+function GuiListIterate()
+
+	local count;
+	local entries, count = GetElementList();
 
 	ramIdx = -1;
 
@@ -256,10 +378,6 @@ function ManipulateElement(guiBehaviour)
 	
 
 	local elementData = elementDatas[typeString];
-	local elementSettings = nil;
-	if settings.overkillMode then
-		elementSettings = GetElementSettings(typeString);
-	end
 
 	if not elementData then
 		elementData = {};
@@ -269,6 +387,12 @@ function ManipulateElement(guiBehaviour)
 	end
 	if not elementData.anchor then
 		elementData.anchor = "LeftTop";
+	end
+
+	
+	local elementSettings = nil;
+	if settings.overkillMode or elementData.isComplex then
+		elementSettings = GetElementSettings(typeString);
 	end
 
 	local anchorIdx;
@@ -290,27 +414,20 @@ function ManipulateElement(guiBehaviour)
 		end
 		if anchorIdx > 5 then
 			tmpScale = settings.bottomRightScale
+		elseif anchorIdx > 2 then
+			tmpScale = settings.centerScale;
 		end
+
 		
-		if elementData.isWireB then
-			tmpScale = settings.wirebugScale;
-			
-		elseif elementData.isMap then
+		if elementData.isMap then
 			tmpScale = settings.mapScale;
 		end		
 	end
-
 
 	invScale = 1 - tmpScale;
 	local scalePosX = baseWidth * invScale / tmpScale;
 	local scalePosY = baseHeight * invScale / tmpScale;
 
-	if elementData.isGlaive then
-		HandleInsectGlaiveUI(guiBehaviour);
-		return;
-	end
-
-	
 	local x = 0;
 	local y = 0;
 	if elementData.useScale then
@@ -326,15 +443,13 @@ function ManipulateElement(guiBehaviour)
 	end
 
 
-	if elementData.isTargetElement then
-		CorrectReticlePos(_tgCameraUIAimPanel:get_data(guiBehaviour));
-	end			
-
-	if elementData.offsetX then
-		x = x + elementData.offsetX * scalePosX;
+	if elementSettings and elementSettings.subPanels then
+		for panelName, subPanel in pairs(elementSettings.subPanels) do
+			HandleSubPanel(guiBehaviour, subPanel, panelName);
+		end
 	end
 
-	if elementSettings then
+	if elementSettings and settings.overkillMode then
 		x = x + elementSettings.posX;
 		y = y + elementSettings.posY;
 	end
@@ -343,54 +458,201 @@ function ManipulateElement(guiBehaviour)
 end
 
 
+function HandleSubPanel(behaviour, subPanel, subPanelName)
+	
+	if subPanel.useGlobalScale then
+		if subPanel.anchor > 5 then
+			tmpScale = settings.bottomRightScale;
+		elseif subPanel.anchor > 2 then
+			tmpScale = settings.centerScale;
+		else
+			tmpScale = settings.uiScale;
+		end
+	else
+		tmpScale = subPanel.scale;
+	end
+	invScale = 1 - tmpScale;
 
-
-function HandleInsectGlaiveUI(behaviour)
-	--this is so annoying
-	local tp = glaiveTopPanel:get_data(behaviour);
+	local tp = behaviour:get_field(subPanelName);
 	set_Scale:call(tp, Vector4f.new(tmpScale, tmpScale, 1.0, 1.0));
 
-	local oX = baseWidth * 0.15 * invScale;
-	local oY = baseHeight * 0.1 * invScale;
+	local dir = subDirScales[subPanel.anchor];
+	local oX = (dir[1] * baseWidth *  subPanel.posAdjustX * invScale);
+	local oY = (dir[2] * baseHeight * subPanel.posAdjustY * invScale);
 
-	-- local pos = ;
-	local offPos = vecSub:call(nil, 
-		get_Position:call(tp),
-	 	Vector4f.new(oX, oY, 1.0, 1.0)
-	);
+	if settings.overkillMode or subPanel.absolutePos then
+		oX = oX + subPanel.posX;
+		oY = oY + subPanel.posY;
+	end
+
+	local offPos;
+	if subPanel.absolutePos then
+		offPos = Vector4f.new(oX, oY, 1.0, 1.0);
+	else
+		offPos = vecAdd:call(nil, 
+			get_Position:call(tp),
+		 	Vector4f.new(oX, oY, 1.0, 1.0)
+		);
+	end	
 
 	set_Position:call(tp, offPos);
 end
 
-function CorrectReticlePos(reticle)
 
-	local reticlePos = get_Position:call(reticle);
 
-	local rX = -(baseWidth * invScale / tmpScale) * aspect;
-	local rY = (baseHeight * invScale / tmpScale) * aspect;
 
-	local addPos = vecAdd:call(nil, reticlePos, Vector4f.new(rX, rY, 0.0, 1.0));	
-	set_Position:call(reticle, addPos);
+
+
+
+------------------------------------INSANE UI GARBAGE----------------------------------------------
+
+function DrawElementSelector()
+
+	local count;
+	local entries, count = GetElementList();
+	local behaviourNames = {""};
+
+	for i=0, count do
+		local bList = value:get_data(entries[i]);
+		if not bList then
+			goto continue;
+		end
+		
+		local guiBaseRootBehaviours = mItems:get_data(bList);
+		local behaviour = guiBaseRootBehaviours[0];
+
+		if behaviour then
+			local bName = behaviour:get_type_definition():get_full_name();
+			if not elementDatas[bName] then
+				table.insert(behaviourNames, bName);
+			end
+		end
+
+		::continue::
+	end
+
+	local value;
+	changed, value = imgui.combo("GUI Elements", 0, behaviourNames);
+	if changed and value > 1 then
+		--selected panel from drop down so add it to the list
+		local bName = behaviourNames[value];
+		local newGuy = {
+			displayName = GetElementClassDisplayName(bName);
+			anchor = 0;
+		};
+		elementDatas[bName] = newGuy;
+		settings.elementSettings[bName] = newGuy;
+		SortElements();
+	end
 end
 
-
-
-function DrawElementSettings(element)
-	imgui.text(element.displayName);
-	imgui.spacing();
-	imgui.begin_rect();
+function DrawPositioningUI(element)
 	local anchorName = dirScales[element.anchor][3];
-	changed, element.scale = imgui.slider_float("Scale: "..element.displayName, element.scale, 0, 1);
-	changed, element.posX = imgui.slider_float("X Pos: "..element.displayName, element.posX, -2000, 2000);
-	changed, element.posY = imgui.slider_float("Y Pos: "..element.displayName, element.posY, -2000, 2000);
-
-	changed, element.anchor = imgui.slider_int("Anchor: "..element.displayName, element.anchor, 0, 8);
+	changed, element.scale = imgui.slider_float("Scale", element.scale, 0, 1);
+	changed, element.posX = imgui.slider_float("X Pos" , element.posX, -2000, 2000);
+	changed, element.posY = imgui.slider_float("Y Pos" , element.posY, -2000, 2000);
+	changed, element.anchor = imgui.slider_int("Anchor:", element.anchor, 0, 8);
 	imgui.same_line();
 	imgui.text(anchorName);
-
-	imgui.end_rect(5);
-	imgui.new_line();
 end
+
+function DrawElementSettings(element, typeName)
+
+	if imgui.tree_node(element.displayName) then
+
+		imgui.same_line();
+		if imgui.button("Remove Element") then
+			elementDatas[typeName] = nil;
+			settings.elementSettings[typeName] = nil;
+			SortElements();
+		end
+		
+		imgui.spacing();
+		imgui.begin_rect();
+
+		DrawPositioningUI(element);
+
+		imgui.new_line();
+		imgui.text("Sub Panels");
+		imgui.spacing();
+
+		local elementType = sdk.find_type_definition(typeName);
+		if elementType then
+
+			local fields = elementType:get_fields();
+			local fieldNames = {""};
+
+			if not element.subPanels then
+				element.subPanels = {};
+			end
+
+			for i, field in ipairs(fields) do
+				if field:get_type() == panelType and not element.subPanels[field:get_name()] then
+					table.insert(fieldNames, field:get_name());
+				end
+			end
+
+			--draw panel selection dropdown
+			local value;
+			changed, value = imgui.combo("Panel List ", 0, fieldNames);
+			if changed and value > 1 then
+				--selected panel from drop down so add it to the list
+				local panelName = fieldNames[value];				
+				element.subPanels[panelName] = {
+					displayName = panelName:gsub("pnl_", "");
+					anchor = element.anchor;
+					posX = 0;
+					posY = 0;
+					posAdjustX = 0.16;
+					posAdjustY = 0.1;
+					scale = 1;
+				};
+			end
+
+
+			--draw individual sub panel ui
+			if element.subPanels then
+				for panelName, subPanel in pairs(element.subPanels) do
+
+					imgui.spacing();
+
+					if not subPanel.displayName then
+						subPanel.displayName = panelName;
+					end						
+
+					if imgui.tree_node(subPanel.displayName.."  - SubPanel") then
+
+						imgui.same_line();
+						if imgui.button("Remove") then
+							element.subPanels[panelName] = nil;
+						end
+
+						imgui.begin_rect();
+						DrawPositioningUI(subPanel);
+						changed, subPanel.posAdjustX = imgui.slider_float("Pos Adjust X", subPanel.posAdjustX, 0, 1);
+						changed, subPanel.posAdjustY = imgui.slider_float("Pos Adjust Y", subPanel.posAdjustY, 0, 1);
+						changed, subPanel.useGlobalScale = imgui.checkbox("Use Global Scale", subPanel.useGlobalScale);
+						changed, subPanel.absolutePos = imgui.checkbox("Absolute Position", subPanel.absolutePos);
+						imgui.end_rect(5);
+
+						imgui.spacing();
+						imgui.tree_pop();
+					end
+				end
+			end
+
+
+		else
+			re.msg("Bad UI Type: "..typeName);
+		end
+		
+		imgui.end_rect(5);
+		imgui.tree_pop();
+	end
+
+	imgui.spacing();
+end
+
 
 re.on_draw_ui(function()
 
@@ -398,10 +660,14 @@ re.on_draw_ui(function()
 	
     if imgui.tree_node("UI Scaler") then
 
-		changed, settings.uiScale = imgui.slider_float("Top Left Scale", settings.uiScale, 0, 1);
+		changed, settings.uiScale = imgui.slider_float("Left Scale", settings.uiScale, 0, 1);
+		changed, settings.centerScale = imgui.slider_float("Center Scale", settings.centerScale, 0, 1);
 		changed, settings.bottomRightScale = imgui.slider_float("Right Scale", settings.bottomRightScale, 0, 1);
 		changed, settings.mapScale = imgui.slider_float("Map Scale", settings.mapScale, 0, 1);
-		changed, settings.wirebugScale = imgui.slider_float("WireBug Scale", settings.wirebugScale, 0, 1);
+
+		--debug
+		-- changed, ram = imgui.slider_int("ram", ram, 0, 100);
+		-- imgui.text(curType);
 
 		imgui.spacing()
 		changed, settings.overkillMode = imgui.checkbox("Overkill Mode", settings.overkillMode);
@@ -410,26 +676,34 @@ re.on_draw_ui(function()
 			if imgui.tree_node("Overkill Settings") then
 
 				imgui.spacing();
-				for i, key in ipairs(elementIdxs) do
-					DrawElementSettings(GetElementSettings(key));
+				if imgui.tree_node("Currently Available GUI Element Selector") then
+					DrawElementSelector();
+					imgui.tree_pop();
 				end
 
+				imgui.new_line();
+
+				imgui.begin_rect();
+				imgui.spacing();
+				for i, key in ipairs(elementIdxs) do
+					DrawElementSettings(GetElementSettings(key), key);
+				end
+				imgui.end_rect(5);
+				
 				imgui.tree_pop();
 			end
 		end
-
-
-		--debug
-		-- changed, ram = imgui.slider_int("ram", ram, 0, 100);
-		-- imgui.text(curType);
 		
-		imgui.spacing();
+		imgui.new_line();
         imgui.tree_pop();
     end
 
 end)
 
 
+-- re.on_application_entry("RenderGUI", function()
+-- 	GuiListIterate();
+-- end)
 
 re.on_pre_gui_draw_element(function(element, context)
 
